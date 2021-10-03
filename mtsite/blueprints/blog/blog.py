fb807fadbf196a5469684621ba13a3cae3d786a2
Alex Ramsdell <alexramsdell@gmail.com>
2021-10-03 19:51:25 -0400

    blog post template and layout wip.

--- mtsite/blueprints/blog/blog.py
+++ mtsite/blueprints/blog/blog.py
@@ -5,5 +5,9 @@ bp = Blueprint('blog', __name__, template_folder='templates', static_folder='sta
 from mtsite.blueprints.blog.posts import posts 
 
 @bp.route('/')
-def index():
+def blog():
     return render_template('blog/index.html', posts=posts)
+
+@bp.route('/post/<int:post_id>')
+def post(post_id):
+    return render_template('blog/post.html', post=posts[post_id])
